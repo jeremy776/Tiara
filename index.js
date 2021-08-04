3f90beb7e6dcad80fca6d32334410a3e8c4de5a9
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-04 15:52:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
 app.get("/", function(req, res) {
