076586bcdc10f99272ec58c27f7770b964d8bf8b
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-04 21:12:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Express = require("express");
 const App = Express();
 const Title = "Night Rest";
 
+App.use(Express.static("./public"));
+
 App.get("/", function(Req, Res) {
   Res.render("index.ejs", {
     Req: Req,
