5fc0a86e33114d60de4480eb351363cc268d73c1
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-04 15:10:01 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.get("/", function(req, res) {
+  res.render("index.html", {
+    req:req,
+    res:res
+  });
+});
+
+app.listen(3000);
