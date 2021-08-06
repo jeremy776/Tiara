deea753d184af99b3184a5b9166503ea05b6bed0
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-06 21:48:48 +0700

    Update AvatarCommand.js

--- src/Commands/Utility/AvatarCommand.js
+++ src/Commands/Utility/AvatarCommand.js
@@ -8,7 +8,23 @@ module.exports = class AvatarCommand extends Command {
       description: {
         content: "Melihat avatar dari user/member lain"
       },
-      cooldown: 3000
+      cooldown: 3000,
+      args: [
+        {
+          id: "user",
+          match: "phrase",
+          default: m => m.author
+        }
+      ]
     });
+  }
+
+  async exec(msg, {user}) {
+
+    let embed = this.client.embed("default")
+    .setImage(user.displayAvatarURL)
+    .setAuthor(`Avatar from ${user}`)
+    return msg.channel.send(embed);
+
   }
 }
