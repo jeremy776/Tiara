e663c3952653b540ae750ffcd3ee36882052c7e3
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-06 15:30:06 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const App = Express();
 const DiscordClient = require('./Structures/Client');
 const Client = new DiscordClient();
 Client.run();
-const Title = Client.user.tag;
+const Title = Client;
 
 App.use(Express.static("./public/"));
 
 App.get("/", function(Req, Res) {
+  console.log(Client);
   Res.render("index.ejs", {
     Req: Req,
     Res: Res,
