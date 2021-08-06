e78350a598c5a4db1dc370d6a69bdbfd50701ec5
Christian <58212127+Christian-123-droid@users.noreply.github.com>
2021-08-06 11:49:26 +0700

    Update HelpCommand.js

--- src/Commands/Utility/HelpCommand.js
+++ src/Commands/Utility/HelpCommand.js
@@ -23,7 +23,7 @@ module.exports = class HelpCommand extends Command {
       if (!command) {
         const embed = this.client.Embed('info')
           .addField(`${this.client.user.username} command's`, `${this.client.config.prefix}help [command]`);
-        for (const category of this.handler.categories.values()) {
+        for (const category of this.commandHandler.categories.values()) {
           embed.addField(category, `${category.filter((cmd) => cmd.aliases.length > 0).map((cmd) => `\`${cmd.aliases[0]}\``).join(', ')}`);
         }
         return msg.channel.send(embed);
