d6bfc9419139728afb844f45af97bd6d5777aa23
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-07 20:51:32 +0700

    fix navbar when click the 🍔

--- public/navbar/index.js
+++ public/navbar/index.js
@@ -3,8 +3,10 @@ window.addEventListener("scroll", function() {
   nav.classList.toggle("NewStyleNavbar", window.scrollY > 0);
 });
 
-window.addEventListener("click", function() {
-  document.querySelector(".hamburger").classList.toggle("on");
-  document.querySelector(".headerList").classList.toggle("on");
-  console.log("click")
+window.addEventListener("load", function() {
+  document.querySelector(".hamburger").addEventListener("click", function() {
+    document.querySelector(".hamburger").classList.toggle("on");
+    document.querySelector(".headerList").classList.toggle("on");
+    console.log("click")
+  });
 });
