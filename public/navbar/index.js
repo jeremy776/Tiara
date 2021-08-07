8e9ce2d0ef744ee45c1b10ac7a4728b36562ed60
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-07 15:31:17 +0700

    Update index.js

--- public/navbar/index.js
+++ public/navbar/index.js
@@ -1,3 +1,9 @@
+document.querySelector(".hamburger").addEventListener("click", function() {
+  document.querySelector(".hamburger").classList.toggle("on");
+  document.querySelector(".headerList").classList.toggle("on");
+  console.log("click")
+});
+  
 window.addEventListener("scroll", function() {
   const nav = document.querySelector("nav");
   nav.classList.toggle("NewStyleNavbar", window.scrollY > 0);
