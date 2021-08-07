7c1214f46f4ffd2cd139defdd5c384c2617ccc96
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-07 21:24:26 +0700

    Menutup navbar ketika klik di luar navbar

--- public/navbar/index.js
+++ public/navbar/index.js
@@ -4,6 +4,14 @@ window.addEventListener("scroll", function() {
 });
 
 window.addEventListener("load", function() {
+
+  document.onclick = (element) => {
+    if(element.target.class !== "hamburger") {
+      document.querySelector(".hamburger").classList.remove("on");
+      document.querySelector(".headerList").classList.remove("on");
+    }
+  }
+
   document.querySelector(".hamburger").addEventListener("click", function() {
     document.querySelector(".hamburger").classList.toggle("on");
     document.querySelector(".headerList").classList.toggle("on");
