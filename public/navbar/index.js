79b6d7261a10c29c1d74458fdbbfbdf4c062ed99
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-07 15:42:07 +0700

    Update index.js

--- public/navbar/index.js
+++ public/navbar/index.js
@@ -1,10 +1,10 @@
+window.addEventListener("scroll", function() {
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("NewStyleNavbar", window.scrollY > 0);
+});
+
 document.querySelector(".hamburger").addEventListener("click", function() {
   document.querySelector(".hamburger").classList.toggle("on");
   document.querySelector(".headerList").classList.toggle("on");
   console.log("click")
-});
-  
-window.addEventListener("scroll", function() {
-  const nav = document.querySelector("nav");
-  nav.classList.toggle("NewStyleNavbar", window.scrollY > 0);
 });
