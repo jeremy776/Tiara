b4fa8a63c19bff79eab4bc71cd33f031d35ab8fb
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-08 23:12:55 +0700

    Quote command yang di terjemahkan ke dalam bahasa Indonesia

---
+++ src/Commands/Fun/QuoteCommand.js
@@ -0,0 +1,28 @@
+const { Command } = require("discord-akairo");
+const translate = require("translatte");
+
+module.exports = class QuoteCommands extends Command {
+  constructor() {
+    super("quote", {
+      aliases: ["quote"],
+      cooldown: 5000,
+      category: "Fun",
+      description: {
+        content: "Memberikan quote terkenal"
+      }
+    })
+  }
+
+  async exec(msg) {
+    try {
+      const baseURL = await axios.get(`${this.client.apijov}/v1/fun/quote`);
+      translate(baseURL.data.content, {to: "id"}).then(res => {
+        return msg.channel.send(this.client.embed("default", `***"${res.text}"***\n\n*- ${baseURL.data.author}*`));
+      }).catch(e => {
+        return msg.channel.send(`\`\`\`js\n${e.message}\`\`\``);
+      });
+    } catch(e) {
+      return msg.channel.send(`\`\`\`js\n${e.message}\`\`\``);
+    }
+  }
+}
