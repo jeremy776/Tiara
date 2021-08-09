29f259543bd27ef450b93d6bc008d6f3821adf00
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-09 07:46:06 +0700

    Update CreateAccount.js

--- src/Utils/CreateAccount.js
+++ src/Utils/CreateAccount.js
@@ -1,1 +1,11 @@
-module.exports
+function CreateAccount(msg) {
+  let data = {
+    id: msg.author.id,
+    uang: 0,
+    bank: 0,
+    role: "user",
+  }
+  return this.client.accountDB.set(msg.author.id, data);
+}
+
+module.exports = { CreateAccount }
