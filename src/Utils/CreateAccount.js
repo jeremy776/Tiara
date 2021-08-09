5f385320791e64533da29f4226d11b6a6b0017e2
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-09 08:09:16 +0700

    Update CreateAccount.js

--- src/Utils/CreateAccount.js
+++ src/Utils/CreateAccount.js
@@ -3,6 +3,7 @@ function CreateAccount(msg) {
     id: msg.author.id,
     uang: 0,
     bank: 0,
+    pekerjaan: null,
     role: "user",
   }
   return this.client.accountDB.set(msg.author.id, data);
