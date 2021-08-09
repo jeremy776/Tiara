7162b79bb5ba7b4c07fd8ced6a290917297319a7
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-09 07:54:44 +0700

    Create DompetCommand.js

---
+++ src/Commands/Economy/DompetCommand.js
@@ -0,0 +1,25 @@
+const { Command } = require("discord-akairo");
+
+module.exports = class DompetCommand extends Command {
+  constructor() {
+    super("dompet", {
+      aliases: ["dompet", "wallet", "balance", "bal"],
+      cooldown: 3000,
+      category: "Economy",
+      description: {
+        content: "cek isi dompet kamu atau orang lain"
+      }
+    })
+  }
+
+  async exec(msg) {
+    let embed = this.client.embed("default")
+    .setAuthor(`Isi dompet dari ${user.username}`, user.displayAvatarURL({dynamic: trie}));
+
+    let data = await this.client.accountDB(msg.author.id);
+    if(!data) {
+      embed.setDescription(`**Uang**: Rp.**0**\n**Bank**: Rp.**0**`);
+      return msg.channel.send(embed)
+    }
+  }
+}
