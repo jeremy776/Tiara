05c7c35ef1f83ab83da37a8dc30af05354d1b3e7
Jeremy <62273120+jeremy776@users.noreply.github.com>
2021-08-10 20:44:10 +0700

    copas lagi

---
+++ src/Commands/Fun/GayCommand.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord-akairo');
+
+module.exports = class GayCommand extends Command {
+  constructor() {
+    super("gay", {
+      aliases: ['gay'],
+      category: 'Fun',
+      cooldown: 3000,
+      description: {
+        content: 'jangan gay, gk baik tau'
+      },
+      args: [
+        {
+          id: 'user',
+          match: 'phrase',
+          type: 'user',
+          default: m => m.author
+        }
+      ]
+    })
+  }
+  async exec(msg, { user }) {
+    try {
+      let baseURL = this.client.apijov + '/v1/canvas/gay?image='+user.displayAvatarURL({format: 'png', size: 2048});
+      let message = await msg.channel.send('Mohon tunggu...');
+      return msg.channel.send({files:[{attachment:baseURL, name:'gay.png'}]}).then(a => message.delete());
+    } catch(e) {
+      return msg.channel.send(`\`\`\`js\n${e.message}\`\`\``);
+    }
+  }
+}
